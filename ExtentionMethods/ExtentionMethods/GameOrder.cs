3d306f220330fe15864dab7e7352dee5e2f7baf3
Acquilla Allen <1stladyofkrypton@gmail.com>
2016-11-27 20:35:10 -0500

    Redid

---
+++ ExtentionMethods/ExtentionMethods/GameOrder.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionMethods
+{
+    public static class Divide
+    {
+        public static int DivideByFive(this int num)
+        {
+            return num / 5;
+        }
+    }
+
+   public static class CountWordClass
+    {
+     public static int CountVowels(this string oGString)
+        {
+            int count = 0;
+
+            foreach (char c in oGString)
+            {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                    count++;
+            }
+
+            return count;
+        }
+    }
+
+    public static class PointFive
+    {
+        public static double TimesByPointFive(this double num)
+        {
+            return num * 0.5;
+        }
+    }
+
+    public static class CheckForS
+    {
+        public static string SCheck(this char nextChar)
+        {
+            if (nextChar == 's')
+            {
+                return "You found s";
+            }
+             else
+            {
+                return "Keep looking";
+            }   
+        }
+    }
+
+    public static class BoolExt
+    {
+        public static int returnAge(this bool var)
+        {
+            if (var == true)
+            {
+                return 29;
+            }
+            else
+            {
+                return 30;
+            }
+        }
+    }
+
+    public static class FloatExt
+    {
+        public static double divide(this float num)
+        {
+            Console.WriteLine("Enter the num to be divided");
+            string a = Console.ReadLine();
+
+            return num / (Convert.ToDouble(a));
+        }
+    }
+        
+}
---
+++ ExtentionMethods/ExtentionMethods/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "The quick brown fox jumped over the lazy dog";
+
+            int wvowels = myString.CountVowels();
+            Console.WriteLine(wvowels);
+
+            int number = 55.DivideByFive();
+            Console.WriteLine(number);
+
+            double newNumber = (1001.05).TimesByPointFive();
+            Console.WriteLine(newNumber);
+
+            char aletter = 'a';
+            Console.WriteLine(aletter.SCheck());
+
+            bool check = true;
+            Console.WriteLine(check.returnAge());
+
+            float anotherNumber = 25.5F;
+            Console.WriteLine(anotherNumber.divide());
+
+            Console.ReadLine();
+        }
+    }
+}
